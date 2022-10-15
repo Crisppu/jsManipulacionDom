1732f603e292f25ec1dc121bf9148d0df43d6f90
Crisppu <cristobalyat69@hotmail.com>
2022-10-14 21:58:16 -0600

    clase 01 completada

---
+++ assets/mio.js
@@ -0,0 +1,5 @@
+/*mi explicacion*/
+/*el document : nos referimos a todo nuestro html osea a todo nuestro documento.html */
+/* .querySelector("span") : seleccionamos el elemento al cual vamos hacer un tiro de cambio*/
+/*.textContent="todo llo que estaba dentro de .task cámbiame por este que acabo de escribir"; */
+document.querySelector("span").textContent="todo llo que estaba dentro de .task cámbiame por este que acabo de escribir";
